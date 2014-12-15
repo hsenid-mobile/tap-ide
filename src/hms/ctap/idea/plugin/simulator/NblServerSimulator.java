3b593948a627ffab957e5152d17e79bfc70add57
isuruw <isuruw@hsenidmobile.com>
2014-12-15 14:59:54 +0600

    Nbl rest simulator added.

---
+++ src/hms/ctap/idea/plugin/simulator/NblServerSimulator.java
@@ -0,0 +1,59 @@
+package hms.ctap.idea.plugin.simulator;
+
+import com.google.common.io.CharStreams;
+import com.google.gson.Gson;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class NblServerSimulator {
+
+    private Queue<String> messageQueue = new ConcurrentLinkedQueue<String>();
+
+    public NblServerSimulator() throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(7000), 0);
+        server.createContext("/sms/send", new MyHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
+    }
+
+    static class MyHandler implements HttpHandler {
+        public void handle(HttpExchange t) throws IOException {
+            String request = CharStreams.toString(new InputStreamReader(t.getRequestBody()));
+
+            Gson gson = new Gson();
+            SmsMtRequestMessage smsMtRequestMessage = gson.fromJson(request, SmsMtRequestMessage.class);
+            System.out.println(smsMtRequestMessage.getMessage());
+
+            String response = "{\n" +
+                    "  \"statusCode\": \"S1000\",\n" +
+                    "  \"requestId\": \"101307311109540017\",\n" +
+                    "  \"statusDetail\": \"Request was successfully processed\",\n" +
+                    "  \"destinationResponses\": [\n" +
+                    "    {\n" +
+                    "      \"statusCode\": \"S1000\",\n" +
+                    "      \"timeStamp\": \"20130731110954\",\n" +
+                    "      \"address\": \"tel: 947712345678\",\n" +
+                    "      \"statusDetail\": \"Request was successfully processed\",\n" +
+                    "      \"messageId\": \"101307311109540017\"\n" +
+                    "    }\n" +
+                    "  ],\n" +
+                    "  \"version\": \"1.0\"\n" +
+                    "}";
+            t.setAttribute("Content-Type", "application/json");
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+
+    public String removeMessage() {
+        return messageQueue.poll();
+    }
+}
--- src/hms/ctap/idea/plugin/ui/CtapSimulatorWindowFactory.java
+++ src/hms/ctap/idea/plugin/ui/CtapSimulatorWindowFactory.java
@@ -1,14 +1,14 @@
 package hms.ctap.idea.plugin.ui;
 
-import com.ctap.simulator.SimulatorServer;
-import com.ctap.simulator.SmsNcsRestService;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
+import hms.ctap.idea.plugin.simulator.NblServerSimulator;
 
 import javax.swing.*;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,24 +23,34 @@ public class CtapSimulatorWindowFactory implements ToolWindowFactory {
 /*        UssdUiFactory ussdUiFactory = new UssdUiFactory(this.toolWindowContent);
         this.toolWindowContent = ussdUiFactory.createInitialUI();*/
 
-        SmsUiFactory smsUiFactory = new SmsUiFactory(this.toolWindowContent);
+        final SmsUiFactory smsUiFactory = new SmsUiFactory(this.toolWindowContent);
         this.toolWindowContent = smsUiFactory.createInitialUI();
         intializedUIs.put(SMS_UI_FACTORY, smsUiFactory);
-        SimulatorServer simulatorServer = new SimulatorServer();
-        simulatorServer.start(null);
 
-        while (true) {
+        try {
+            final NblServerSimulator nblServerSimulator = new NblServerSimulator();
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
 
-            try {
-                Thread.sleep(1000);
-                String message = SmsNcsRestService.removeMessage();
-                smsUiFactory.createMsgReceivedUI(message);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+                        try {
+                            Thread.sleep(1000);
+                            String message = nblServerSimulator.removeMessage();
+                            if(message != null) {
+                                smsUiFactory.createMsgReceivedUI(message);
+                            }
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+            thread.start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-
     }
 
     public static Object getInitializedUIObject(String name) {
