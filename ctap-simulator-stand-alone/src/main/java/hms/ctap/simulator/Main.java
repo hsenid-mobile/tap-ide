be3070d4e2d5b0a74ddf0c9b4a3bd94fc1d2b3c1
Oshan Deshapriya <oshand@hsenidmobile.com>
2015-01-13 18:59:49 +0530

    Added application URL dropdown

--- ctap-simulator-stand-alone/src/main/java/hms/ctap/simulator/Main.java
+++ ctap-simulator-stand-alone/src/main/java/hms/ctap/simulator/Main.java
@@ -1,27 +1,48 @@
 package hms.ctap.simulator;
 
 import javax.swing.*;
-
 import hms.ctap.simulator.ui.SmsUiFactory;
-
-
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main extends JFrame {
     static SmsUiFactory smsUiFactory;
+    private String applicationURL;
+    private JLabel applicationURLLabel;
 
     public Main() {
-        JPanel jPanel = new JPanel();
-        smsUiFactory = new SmsUiFactory(jPanel);
-        smsUiFactory.createInitialUI();
+        this.applicationURLLabel = new JLabel("Hello world");
+        JPanel contentPanel = new JPanel();
+        smsUiFactory = new SmsUiFactory(contentPanel);
+
+        String[] applicationList = {"SMS: http://localhost:8080/helloWord","USSD: http://localhost:8081/helloWord-"};
+        final JComboBox applicationDropDown = new JComboBox(applicationList);
+        applicationDropDown.addActionListener (new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                applicationURLLabel.setText(applicationDropDown.getSelectedItem().toString());
+            }
+        });
+        applicationDropDown.setPreferredSize(new Dimension(500,20));
+
+        SpringLayout bottomLayerLayout = new SpringLayout();
+        JPanel bottomLayer = new JPanel();
+        JLabel appListLable = new JLabel("Applications");
+
+        bottomLayer.add(appListLable,BorderLayout.WEST);
+        bottomLayer.add(applicationDropDown);
+//        bottomLayer.add(applicationURLLabel,BorderLayout.WEST);
+
+//        bottomLayerLayout.putConstraint(SpringLayout.WEST, applicationDropDown, 200, SpringLayout.WEST, appListLable);
+
 
         Container pane = getContentPane();
-
         pane.add(smsUiFactory.createInitialUI());
+        pane.add(bottomLayer,BorderLayout.SOUTH);
 
 
-        setTitle("Simple example");
-        setSize(600, 650);
+        setTitle("hSenid Mobile - CTAP Simulator");
+        setSize(600, 680);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
